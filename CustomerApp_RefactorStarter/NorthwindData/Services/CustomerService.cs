a516cc0daf0e543dd932427607ef1aec16ebd5e2
swase <fg2g15@soton.ac.uk>
2021-09-09 10:38:55 +0100

    Added customerService interface

---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.Services
+{
+    class CustomerService : ICustomerService
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerService(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public CustomerService()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public void CreateCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomerById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomersList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomerChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NorthwindData.Services
+{
+    interface ICustomerService
+    {
+        List<Customer> GetCustomersList();
+        public Customer GetCustomerById();
+        public void CreateCustomer(Customer c);
+        public void SaveCustomerChanges();
+        public void RemoveCustomer(Customer c);
+    }
+}
